72e3fcf21ccedc2cfd5cc4d6532243977aa3b47a
Rodrigo Samayoa <jorosafi@gmail.com>
2018-01-24 11:00:02 -0800

    testing github

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,11 @@
-var gulp = require('gulp');
+var gulp = require('gulp') // load gulp first
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename');
 
-gulp.task('default', defaultTask);
+gulp.task('default', function(){
+  gulp.src('js-files/*.js')
+    .pipe(uglify()) //call uglify function on files
+    .pipe(rename({extname: '.min.js'}))//renaming the new ugly file
+    .pipe(gulp.dest('build/js'))
+});
 
-function defaultTask(done) {
-  // place code for your default task here
-  done();
-}
---
+++ js-files/index.js
@@ -0,0 +1,2 @@
+var test = 2
+console.log('this is a test' + test);
