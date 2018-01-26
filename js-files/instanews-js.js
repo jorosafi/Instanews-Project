be8c9327f35f088632850a652b95363714b3f6d2
Rodrigo Samayoa <jorosafi@gmail.com>
2018-01-25 21:31:59 -0800

    Added animation Jquery to js file

---
+++ js-files/instanews-js.js
@@ -0,0 +1,9 @@
+//this is the JQuery code that Jim used to animate boxes in his example
+// $(".article")
+//   .hide()
+//   .first()
+//   .show("fast", function showNext() {
+//     $(this)
+//       .next(".article")
+//       .show(500, showNext);
+//   });
