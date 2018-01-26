625c86627270d20587ea01dd45083c7be95247be
Rodrigo Samayoa <jorosafi@gmail.com>
2018-01-26 14:49:14 -0800

    API is set up, but it's not properly hooked to the divs

--- js-files/instanews-js.js
+++ js-files/instanews-js.js
@@ -7,3 +7,40 @@
 //       .next(".article")
 //       .show(500, showNext);
 //   });
+
+
+
+
+$(".topics-dropdown").change(function () {
+
+  var selection = $(".topics-dropdown").val();
+
+  var url = "https://api.nytimes.com/svc/topstories/v2/" + selection + ".json";
+
+  url += '?' + $.param({
+    'api-key': "c5099af9412b4a3eb9e35f2c26f944dd"
+  });
+
+  $.ajax({
+    url: url,
+    method: 'GET',
+  }).done(function (result) {
+
+    console.log(result.results);
+
+    $.each(result, function(key, value){
+
+      var articleIndex = 0;
+      articleIndex ++
+
+      $('.headline').append(result.results[articleIndex].title)
+        .next('.headline');
+    })
+
+  }).fail(function (err) {
+    throw err;
+  });
+});
+
+
+
