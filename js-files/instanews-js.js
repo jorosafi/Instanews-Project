4e4a53aed36e9e6ce0d6d2cac59e90422877040b
Rodrigo Samayoa <jorosafi@gmail.com>
2018-01-28 22:47:36 -0800

    added links to article divs

--- js-files/instanews-js.js
+++ js-files/instanews-js.js
@@ -34,25 +34,30 @@ $('.topics-dropdown').change(function () {
 
     // var filteredResults = result.results.filter(function(key){
     //   if (result.results[key].multimedia.length > 0){
-    //     return result.results[key]
+    //     return result.results[key];
     //   }
-    // })
+    //   })
+
     var slicedResults = result.results.slice(0, 12);
+    // var slicedResults = filteredResults.slice(0, 12);
 
     console.log(slicedResults);
     
 
     $.each(slicedResults, function(key, value){
       var nytHeadline = slicedResults[key].abstract;
-      var nytImg = slicedResults[key].multimedia[4].url;
-      
-      console.log(nytImg);
+      var nytImg = slicedResults[key].multimedia[4].url; 
+      var nytLink =  slicedResults[key].url;
+
+      $('.headline:eq('+key+')').append(nytHeadline);
       
       $('.article:eq('+key+')').css({
         'background':'url("'+nytImg+'")', 'background-size': 'cover',
         'background-position': 'center'});
 
-      $('.headline:eq('+key+')').append(nytHeadline);
+      $('.aricle-link:eq('+key+')').attr('href', nytLink);
+
+      
     })
 
   })
